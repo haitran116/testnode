0d0ab4d232eb745c446cdc529287f5a5427fab08
haitran <haitm.1106@gmail.com>
2024-08-07 11:19:13 +0700

    sss

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'testnode',
+            script: './index.js',
+            env: {
+                PORT: 3000,
+            },
+        },
+    ],
+};
