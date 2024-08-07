e41548124eb0090bcd0ccade5dfc9e03e5088a70
haitran <haitm.1106@gmail.com>
2024-08-07 11:22:58 +0700

    bb

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ const port = 3000;
 // Cấu hình middleware để phục vụ các tệp tĩnh từ thư mục "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/test', (req, res) => {
+    res.json({ user: 'hai' });
+});
+
 // Route cơ bản
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.json({ user: 'geek' });
 });
+
 
 // Khởi động máy chủ
 app.listen(port, () => {
